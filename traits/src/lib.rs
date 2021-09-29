4e36721cdd1d43b2683d26b54444926508b30940
Antonio Antonino <antonio@kilt.io>
2021-09-29 17:02:58 +0200

    wip: experimenting with a different storage migrator

--- traits/src/lib.rs
+++ traits/src/lib.rs
@@ -17,8 +17,6 @@
 // If you feel like getting in touch with us, you can do so at info@botlabs.org
 #![cfg_attr(not(feature = "std"), no_std)]
 
-use frame_support::dispatch::Weight;
-
 /// The sources of a call struct.
 ///
 /// This trait allows to differentiate between the sender of a call and the
@@ -57,10 +55,13 @@ impl<S: Clone, P: Clone> CallSources<S, P> for (S, P) {
 ///
 /// In this way, the migrator can access the pallet's storage and the pallet's
 /// types directly.
-pub trait VersionMigratorTrait<T> {
+pub trait VersionMigratorTrait<T>: Sized {
 	#[cfg(feature = "try-runtime")]
-	fn pre_migrate(&self) -> Result<(), &str>;
-	fn migrate(&self) -> Weight;
+	fn pre_migrate(&self) -> Result<(), String>;
+	fn migrate(&self) -> frame_support::weights::Weight;
+	fn next_version(&self) -> Option<Self> {
+		None
+	}
 	#[cfg(feature = "try-runtime")]
-	fn post_migrate(&self) -> Result<(), &str>;
+	fn post_migrate(&self) -> Result<(), String>;
 }
